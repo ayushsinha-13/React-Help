a4a32e7febf9d565b77b8de806cebd3bb8c8fb8b
Ayush Sinha <ayushsinhajsr20@gmail.com>
2023-04-28 15:53:42 +0530

    added deviceList updates in firebase

--- Assets/Functions/NotificationService.js
+++ Assets/Functions/NotificationService.js
@@ -1,20 +1,37 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
 import {PermissionsAndroid} from 'react-native';
+import database from '@react-native-firebase/database';
+import {useState} from 'react';
 PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 export async function requestUserPermission() {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-  
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-        getFcmToken()
-    }
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+    getFcmToken();
+  }
 }
 
+const addNewDevices = async props => {
+  let length = await database().ref('length/1/value/').once('value');
+  console.log(length);
+
+  length = length.val() + 1;
+  const vaItem = 'world';
+
+  const newDevice = await database().ref(`deviceList/${length}`).set({
+    value: props.newDevice,
+  });
+
+  await database().ref(`length/1`).update({
+    value: length,
+  });
+};
 
 const getFcmToken = async () => {
   let fcmToken = await AsyncStorage.getItem('fcmToken');
@@ -26,33 +43,34 @@ const getFcmToken = async () => {
       if (fcmToken) {
         console.log(fcmToken, ' New Token');
         await AsyncStorage.setItem('fcmToken', fcmToken);
+        addNewDevices({newDevice: fcmToken});
       }
     } catch (error) {
-      console.log(error, " raised in fcmToken");
+      console.log(error, ' raised in fcmToken');
     }
   }
 };
 
-export const notificationListener = async()=>{
-    messaging().onNotificationOpenedApp(remoteMessage => {
+export const notificationListener = async () => {
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+  });
+
+  messaging().onMessage(async remoteMessage => {
+    console.log('reciever in foreground', remoteMessage);
+  });
+
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
         console.log(
-          'Notification caused app to open from background state:',
+          'Notification caused app to open from quit state:',
           remoteMessage.notification,
         );
-      });
-
-      messaging().onMessage(async remoteMessage => {
-        console.log("reciever in foreground", remoteMessage)
-      })
-
-      messaging()
-      .getInitialNotification()
-      .then(remoteMessage => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-        }
-      });
-}
+      }
+    });
+};
